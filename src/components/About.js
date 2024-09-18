97b3a6c1dab72df36a4d818a3cbae896eb1099a1
Aryan Singh <aryansingh3418@gmail.com>
2024-09-18 07:17:08 +0530

    yes

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">About</h1>
+            <p className="text-center">This is the about page</p>
+        </div>
+    )
+}
+export default About;
